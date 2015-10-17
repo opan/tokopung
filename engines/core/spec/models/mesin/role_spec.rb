cb44d51f60c58e296933b431c0339eb7e6b02d6f
opan <opan.neutron@gmail.com>
2015-10-17 21:54:56 +0700

    add some test in model role.rb add factory in model role.rb

--- engines/core/spec/models/mesin/role_spec.rb
+++ engines/core/spec/models/mesin/role_spec.rb
@@ -2,19 +2,36 @@ require "spec_helper"
 
 module Mesin
   describe Role do
-    it "default role super admin can not be deleted" do
+    it "default role super admin test method it_can_be_deleted? must result false" do
       super_admin = FactoryGirl.create(:role, :super_admin)
       expect(super_admin.it_can_be_deleted?).to eq false 
     end
 
-    it "default role admin can not be deleted" do
+    it "default role admin test method it_can_be_deleted? must result false" do
       admin = FactoryGirl.create(:role, :admin)
       expect(admin.it_can_be_deleted?).to eq false
     end
 
-    it "default role customer can not be deleted" do
+    it "default role customer test method it_can_be_deleted? must result false" do
       customer = FactoryGirl.create(:role, :customer)
       expect(customer.it_can_be_deleted?).to eq false
+    end
+
+    it "role can not be deleted if it_can_be_deleted is false and should not raise error when reload" do
+      super_admin = FactoryGirl.create(:role, :super_admin)
+      super_admin.destroy
+      expect {super_admin.reload}.not_to raise_error
+    end
+
+    it "method it_can_be_deleted? should return true if it_can_be_deleted is true" do
+      deleted_role = FactoryGirl.create(:deleted_role)
+      expect(deleted_role.it_can_be_deleted).to eq true
+    end
+
+    it "role can be deleted if it_can_be_deleted is true and raise error when reload" do
+      deleted_role = FactoryGirl.create(:deleted_role)
+      deleted_role.destroy
+      expect {deleted_role.reload}.to raise_error
     end
   end  
 end
--- engines/core/spec/support/mesin/role.rb
+++ engines/core/spec/support/mesin/role.rb
@@ -14,6 +14,11 @@ module Mesin
       trait :customer do
         role_name "customer"
       end
+    end
+
+    factory :deleted_role, class: "Mesin/Role" do |f|
+      f.it_can_be_deleted true
+      role_name "super_admin"
     end   
   end # end FactoryGirl
 end
