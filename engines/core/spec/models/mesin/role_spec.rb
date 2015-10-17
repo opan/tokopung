67906e44c300857cd1f1dd30b2dea340bf386cdd
opan <opan.neutron@gmail.com>
2015-10-17 21:05:02 +0700

    add spec for model role

--- engines/core/spec/models/mesin/role_spec.rb
+++ engines/core/spec/models/mesin/role_spec.rb
@@ -2,8 +2,19 @@ require "spec_helper"
 
 module Mesin
   describe Role do
-    it "has an super admin role user" do
-      expect(FactoryGirl.build(:role, :super_admin)).to be_valid 
+    it "default role super admin can not be deleted" do
+      super_admin = FactoryGirl.create(:role, :super_admin)
+      expect(super_admin.it_can_be_deleted?).to eq false 
+    end
+
+    it "default role admin can not be deleted" do
+      admin = FactoryGirl.create(:role, :admin)
+      expect(admin.it_can_be_deleted?).to eq false
+    end
+
+    it "default role customer can not be deleted" do
+      customer = FactoryGirl.create(:role, :customer)
+      expect(customer.it_can_be_deleted?).to eq false
     end
   end  
 end
