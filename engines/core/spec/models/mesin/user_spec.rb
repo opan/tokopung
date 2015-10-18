39e3a2de6b8aa3d31efdb33438a849142b9dc922
opan <opan.neutron@gmail.com>
2015-10-18 20:34:20 +0700

    add test spec in model user.rb

--- engines/core/spec/models/mesin/user_spec.rb
+++ engines/core/spec/models/mesin/user_spec.rb
@@ -39,14 +39,28 @@ module Mesin
       expect(user.role_users).to be_empty
     end
 
-    it "default role user is customer if role blank" do
+    it "set_default_role method must return role customer if role blank" do
       customer = FactoryGirl.create(:role, :customer)
       FactoryGirl.create(:role, :super_admin)
       user = FactoryGirl.build(:user, :valid_email)
       expect(user.set_default_role).to eq customer.id
     end
 
-    
+    it "role_super_admin_if_table_blank? method must set role as super_admin if table user still blank" do
+      customer = FactoryGirl.create(:role, :customer)
+      super_admin = FactoryGirl.create(:role, :super_admin)
+      user = FactoryGirl.build(:user, :valid_email)
+      expect(user.role_super_admin_if_table_blank?).to eq super_admin.id
+    end
+
+    it "check_role_users method must create record in \n
+      table role_users if role_users with current user id and role id not exist" do
+      customer = FactoryGirl.create(:role, :customer)
+      super_admin = FactoryGirl.create(:role, :super_admin)
+      user = FactoryGirl.create(:user, :valid_email)
+      user.check_role_users
+      expect(user.role_users).not_to be_empty
+    end
 
   end # end describe User
 end
