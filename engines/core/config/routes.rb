bc8c89940b13a43ce5fef4433768c98936a56671
opan <opan.neutron@gmail.com>
2015-12-24 13:54:48 +0700

    add offline google fonts update application layout update dashboard index

--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -2,5 +2,6 @@ Mesin::Core::Engine.routes.draw do
   # if install Devise inside an Engine,
   # make sure to add 'module: :devise' to tell Devise that we're running inside en Engine
   devise_for :users, class_name: "Mesin::User", module: :devise, controllers: {registrations: "mesin/registrations"}
+  
   root to: "dashboards#index"
 end
