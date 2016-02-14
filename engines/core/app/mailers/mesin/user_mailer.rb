d108c1c55b61296f8e41d1dff9e893bc0b3887c1
opan <opan.neutron@gmail.com>
2016-02-14 12:49:20 +0700

    add gem roadie to styling mailer in rails, but still not work. try figure out how to styling mailer in rails

--- engines/core/app/mailers/mesin/user_mailer.rb
+++ engines/core/app/mailers/mesin/user_mailer.rb
@@ -1,8 +1,12 @@
 module Mesin
   class UserMailer < Devise::Mailer
+    # fix issues in roadie-rails
+    # see roadie-rails github
+    self.asset_host = nil
+    
     # include roadie-rails to make inline css
     include Roadie::Rails::Automatic
-    
+
     helper :application
     # as Devise documentation
     include Devise::Controllers::UrlHelpers
@@ -15,5 +19,10 @@ module Mesin
       @token = token
       mail(template_path: "mesin/user_mailer", to: current_user.email, subject: "Unlock Instructions")
     end
+
+    private
+      def roadie_options
+        super unless Rails.env.test?
+      end
   end
 end
